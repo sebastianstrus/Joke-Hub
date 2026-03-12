61be56d3c9ef87c53722fedcf21f03c9417bd6ce
Sebastian Strus <sebastian.strus1@gmail.com>
2026-03-12 20:42:07 +0100

    Initial Commit

---
+++ Joke Hub/Joke_HubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Joke_HubApp.swift
+//  Joke Hub
+//
+//  Created by Sebastian Strus on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Joke_HubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
